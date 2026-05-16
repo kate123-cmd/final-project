0f50563d3150b4af1629d4552ac7aea9dc607ae5
kate123-cmd <kashasingh22@gmail.com>
2026-05-16 11:05:23 -0400

    pylint

--- server.py
+++ server.py
@@ -1,51 +1,54 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
-# Import Flask, render_template, request from the flask pramework package
+"""
+Executing this function initiates the application of sentiment
+analysis to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
+
+# Import Flask, render_template, request from the flask package
 from flask import Flask, render_template, request
+
 # Import the emotion_detector function from the package created
 from EmotionDetection.emotion_detection import emotion_detector
-#Initiate the flask app
+
+# Initiate the flask app
 app = Flask("Emotion Detector")
+
 
 @app.route("/emotionDetector")
 def emot_detector():
+    """
+    Analyze the provided text and return detected emotions.
+    """
+
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Pass the text to the emotion_detector function and store the response
+    # Pass the text to the emotion_detector function
     response = emotion_detector(text_to_analyze)
-    
-     # Check for invalid/blank input
+
+    # Check for invalid/blank input
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    
+
     return (
-    "For the given statement, the system response is "
-    "'anger': {}, "
-    "'disgust': {}, "
-    "'fear': {}, "
-    "'joy': {} and "
-    "'sadness': {}. "
-    "The dominant emotion is {}."
-    ).format(
-        response['anger'],
-        response['disgust'],
-        response['fear'],
-        response['joy'],
-        response['sadness'],
-        response['dominant_emotion']
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, "
+        f"'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, "
+        f"'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
+        f"The dominant emotion is "
+        f"{response['dominant_emotion']}."
     )
-    
+
+
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+    """
+    Render the main application page.
+    """
     return render_template('index.html')
 
+
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="0.0.0.0", port=5000)
